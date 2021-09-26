671d0530ba8c271bfde92948470865661e3df57b
Venger-V <91158292+Venger-V@users.noreply.github.com>
2021-09-26 23:25:03 +0300

    Update 1.2.1.py

--- lesoon 2/1.2.1.py
+++ lesoon 2/1.2.1.py
@@ -3,4 +3,5 @@ day = 'Saturday'
 print(f'Good day {name}! {day} is a perfect day to learn some python.')
 
 print('Good day {}! {} is a perfect day to learn some python.'.format(name, day))
+
 print('Good day %s! %s is a perfect day to learn some python.' % (name, day))
