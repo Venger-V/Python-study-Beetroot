c9a7cb606cf41c40df117c430aa6e490e41a11d8
Venger-V <91158292+Venger-V@users.noreply.github.com>
2021-09-26 23:23:24 +0300

    Add files via upload

---
+++ lesoon 2/1.2.1.py
@@ -0,0 +1,7 @@
+name = 'Vitalii'
+day = 'Saturday'
+print(f'Good day {name}! {day} is a perfect day to learn some python.')
+
+print('Good day {}! {} is a perfect day to learn some python.'.format(name, day))
+
+print('Good day %s! %s is a perfect day to learn some python.' % (name, day))
---
+++ lesoon 2/1.2.2.py
@@ -0,0 +1,8 @@
+f_name = 'Vitalii'
+s_name = 'Venger'
+print('Hello' + ',' + f_name + ' ' + s_name + '!')
+
+f_name = 'Vitalii'
+s_name = 'Venger'
+full_name = f_name + ' ' + s_name
+print('Hello' + ',' + full_name + '!')
