db74d95c4dbacaaad5c2116512612e4ae8c3441f
Venger-V <91158292+Venger-V@users.noreply.github.com>
2021-10-09 16:30:48 +0300

    Update 1.4.2.py

--- lesson4/1.4.2.py
+++ lesson4/1.4.2.py
@@ -4,6 +4,7 @@ lis = []
 lis1 = []
 i = 0
 n = 0
+#  увеличил списки, с n < 10 lis2 может быть пустым
 while i == n < 20:
     lis.append(random.randint(0, 100))
     i += 1
@@ -11,5 +12,3 @@ while i == n < 20:
     n += 1
 lis2 = list(set(lis) & set(lis1))
 print(lis2)
-
-
