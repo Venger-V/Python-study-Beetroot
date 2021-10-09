85ba3c86add6e9ba0bfeba9fc1661bc4d0ff78b7
Venger-V <91158292+Venger-V@users.noreply.github.com>
2021-10-09 17:05:36 +0300

    Create 1.8.1.py

---
+++ lesson8/1.8.1.py
@@ -0,0 +1,11 @@
+def favorite_movie(film_name):
+    print(f'My favorite movie i\'s named {film_name}.')
+
+
+favorite_movie('Friends')
+
+film_name = 'Back To The Future'
+favorite_movie(film_name)
+
+film_name = 'Man In Black'
+favorite_movie(film_name + ' and Matrix')
